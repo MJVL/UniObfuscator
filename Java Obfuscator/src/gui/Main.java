66b563ffaa64513c5baaef0bbd9835f365725c7d
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-17 16:42:30 -0400

    Add GUI main

---
+++ Java Obfuscator/src/gui/Main.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("UniGUI.fxml"));
+        primaryStage.setTitle("UniObfuscator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
