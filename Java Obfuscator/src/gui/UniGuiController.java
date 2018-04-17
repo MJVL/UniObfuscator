808391a9450939c4ca5718225f5e483d8adbb981
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-17 16:42:36 -0400

    Add controller stub

---
+++ Java Obfuscator/src/gui/UniGuiController.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UniGuiController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
