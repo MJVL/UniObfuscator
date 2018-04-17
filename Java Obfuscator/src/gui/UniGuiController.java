59ef10c1c5416af2b8b83cbe580cc389ad10838a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-17 16:57:27 -0400

    Link controller with controls

--- Java Obfuscator/src/gui/UniGuiController.java
+++ Java Obfuscator/src/gui/UniGuiController.java
@@ -1,10 +1,14 @@
 package gui;
 
+import com.jfoenix.controls.JFXTextArea;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UniGuiController implements Initializable {
+
+    public JFXTextArea txtSourceCode;
+    public JFXTextArea txtObfuscatedCode;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
