c94c8016878ac7c27a4cbe69310e7669ed48948d
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-17 17:12:59 -0400

    Misc GUI Updates
    - Added Icon
    - txtObfuscatedCode.editable -> false
    - Raleway -> Monospaced Regular

--- Java Obfuscator/src/gui/Main.java
+++ Java Obfuscator/src/gui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("UniGUI.fxml"));
         primaryStage.setTitle("UniObfuscator");
+        primaryStage.getIcons().add(new Image("file:src/gui/assets/icon.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
