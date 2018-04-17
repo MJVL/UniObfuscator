57aa1e07814de394680dc5a3eefc69d987f39e47
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-16 21:32:50 -0400

    Add Obfuscator class

---
+++ Java Obfuscator/Tester/Obfuscator.java
@@ -0,0 +1,24 @@
+public class Obfuscator {
+
+    public static String obfuscate(String pString) {
+        return obfuscate(pString, "");
+    }
+
+    public static String obfuscate(String pString, String bComment) {
+        return "/*" + bComment + toUnicode("*/") + toUnicode(pString) + toUnicode("/*") + "*/";
+    }
+
+
+    public static String toUnicode(String uString) {
+        String unicode = "";
+        for (char c: uString.toCharArray()) {
+            unicode += toUnicode(c);
+        }
+        return unicode;
+    }
+
+    public static String toUnicode(Character uChar) {
+        return "\\u" + Integer.toHexString(uChar | 0x10000).substring(1);
+    }
+
+}
