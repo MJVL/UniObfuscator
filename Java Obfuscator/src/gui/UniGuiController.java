ccf24f70f7a9367591a662795bc85a18f24f96cd
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-04-17 17:26:53 -0400

    Add option controls
    Comment + TODO JFXCheckBoxes
    Default chkComment -> checked
    Default chkTODO -> unchecked

--- Java Obfuscator/src/gui/UniGuiController.java
+++ Java Obfuscator/src/gui/UniGuiController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.fxml.Initializable;
 import utility.Obfuscator;
@@ -11,9 +12,13 @@ public class UniGuiController implements Initializable {
 
     public JFXTextArea txtSourceCode;
     public JFXTextArea txtObfuscatedCode;
+    public JFXCheckBox chkComment;
+    public JFXCheckBox chkTODO;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        chkComment.setSelected(true);
+        txtObfuscatedCode.setText(Obfuscator.obfuscate(""));
         txtSourceCode.textProperty().addListener((observable, oldText, newText) -> {
             txtObfuscatedCode.setText(Obfuscator.obfuscate(newText));
         });
