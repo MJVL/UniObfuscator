b76ec1fd0ea032023315a70d78433b19a9d87ad6
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-17 19:36:55 -0400

    Fix icon paths to display in executables

--- Java Obfuscator/src/gui/Main.java
+++ Java Obfuscator/src/gui/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import utility.Obfuscator;
 
 public class Main extends Application {
 
@@ -13,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("UniGUI.fxml"));
         primaryStage.setTitle("UniObfuscator");
-        primaryStage.getIcons().add(new Image("file:src/gui/assets/icon.png"));
+        primaryStage.getIcons().add(new Image(getClass().getResource("assets/icon.png").toString()));
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
